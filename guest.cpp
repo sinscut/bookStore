78b31c8bfccab0d6d1d168f9937d02439c145d62
sinscut <sinscut@qq.com>
2017-06-15 16:46:13 +0800

    顾客端已完成. 包括查询和反馈两个功能.

--- guest.cpp
+++ guest.cpp
@@ -16,5 +16,32 @@ void guest::want()
 	cin >> temp.name;
 	cout<< "作者\n$";
 	cin >> temp.author;
-	
+	//查询图书是否在库bool inStock(book);
+	if (inStock(temp))
+		cout << "书库中已经有这本书了哦,试试查询功能吧\n";
+	else
+	{
+		cout << "请留下你的联系方式,该书上架后我们会尽快与你取得联系.\n";
+		cout << "$姓名:";
+		cin >> name;
+		cout << "$电话:";
+		cin >> TEL;
+		fstream file(notification, ios::out);
+		file << "读者反馈:\n"
+			<< "希望上架的图书:" << "《" << temp.name << "》" << "\t" << "作者:" << temp.author << "\n"
+			<< "读者信息:\n"
+			<< "姓名:\t" << name << "电话:\t" << TEL;
+		cout << "登记成功!";
+		//生存期结束自动关闭文件
+	}
+}
+void guest::inquire()
+{
+	int choice;
+	cout << "请选择查找方式:\n\n"
+		<< "<0>分类查找\n"
+		<< "<1>查找书名\n"
+		<< "<2>查找作者\n\n";
+	cin >> choice;
+	commonFind(choice);
 }
--- pubUse.h
+++ pubUse.h
@@ -10,7 +10,7 @@ const string notification = "d:\\notificaiton.txt";//通知文件名
 
 struct book
 {
-	string TP;//顾客界面不显示书号
+	string TP;
 	string category;
 	string name;
 	string author;
@@ -18,6 +18,28 @@ struct book
 	string pos;//位置,例如1A架01层
 	int balance;
 	book() { TP = "0"; category = "0"; name = "0"; author = "0"; intro = "0"; pos = "0"; balance = 0; }
+	void show()//显示全部信息,与其他内容以空行分割
+	{
+		cout << "\n类别: " << category << "书名: " << "《" << name << "》" << "作者: " << author << endl;
+		cout <<"书号: " << TP << "位置: " << pos << endl;
+		cout << "简介: " << intro<<"\n\n";
+		return;
+	}
+	void set()
+	{
+		cout << "$类别: ";
+		cin >> category;
+		cout << "$书名: ";
+		cin >> name; 
+		cout << "$作者: ";
+		cin >> author;
+		cout << "$书号: ";
+		cin >> TP;
+		cout << "$位置: ";
+		cin >> pos;
+		cout << "$简介: ";
+		cin >> intro;
+	}
 };
 
 const book mark;//文件结束标记
@@ -30,7 +52,7 @@ int endMark(book book)
 
 //从库存中查找对应书籍,返回在文件中的位置
 //多本同名书和同名作者如何处理? find函数用处不大, 考虑在需要查找位置功能的函数中逐一实现
-long find(book toBeFound)
+/*long find(book toBeFound)
 {
 	book temp;
 	long pos;
@@ -51,6 +73,73 @@ long find(book toBeFound)
 		cout << "根据所给条件未找到该书\n";
 		return -1;
 	}
-}
+}*/
 //判断库存中是否有要查找的书
-void inStock()
+bool inStock(book toBeFound)
+{
+	book temp;
+	fstream file(stock, ios::in | ios::binary);//打开库存文件
+	file.seekg(0, ios::beg);
+	do
+	{
+		file.read((char*)&temp, sizeof(book));
+		if (temp.name == toBeFound.name && temp.author == toBeFound.author)
+			return true;
+	} while (!endMark(temp));
+	return false;
+	//file生存期结束应该会自动关闭文件
+}
+const int cat = 0;
+const int name = 1;
+const int author = 2;
+//flag为查询方式
+void commonFind(int flag)
+{
+	book temp;
+	int none = 1;
+	string answer;//搜索项
+	if (flag == cat)
+		cout << "你可以输入以下分类:\n"
+			<< "文学\t艺术\t科技\t工业\n$";
+	if (flag == name)
+		cout << "$输入书名:";
+	if (flag == author)
+		cout << "$输入作者名:";
+	cin >> answer;
+	fstream file(stock, ios::in | ios::binary);//打开库存文件
+	file.seekg(0, ios::beg);
+	do
+	{
+		file.read((char*)&temp, sizeof(book));
+		switch (flag)//查找方式
+		{
+		default:
+			break;
+		case cat:
+		{	
+			if (temp.category == answer)
+			{
+				temp.show();
+				none = 0;
+			}
+		}
+		case name:
+		{
+			if (temp.name == answer)
+			{
+				temp.show();
+				none = 0;
+			}
+		}
+		case author:
+		{
+			if (temp.author == answer)
+			{
+				temp.show();
+				none = 0;
+			}
+		}
+		}//switch
+	} while (!endMark(temp));
+	if (none) cout << "未找到相关书籍...\n";
+}
